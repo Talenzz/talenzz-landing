f5cc83fd923ba51863bdd1b78e00b247692df633
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-09-28 12:27:30 +0200

    Add tailwind theme setup
    * add epilogue font and default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                white: "var(--white)",
+                black: "var(--black)",
+                salmon: "var(--salmon)",
+                sky: "var(--sky)",
+                icterine: "var(--icterine)",
+            },
+            fontFamily: {
+                sans: [
+                    "Epilogue",
+                    "sans-serif",
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
+        },
+    },
+    plugins: [],
+};
