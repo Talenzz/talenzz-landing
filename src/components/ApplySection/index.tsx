71c21d63e0a66720f3c46aa3cdfc42c21eb18a71
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-09-30 22:58:36 +0200

    Fix ApplySection spacing

--- src/components/ApplySection/index.tsx
+++ src/components/ApplySection/index.tsx
@@ -10,14 +10,14 @@ interface ApplySectionProps {
 
 export function ApplySection({ content }: ApplySectionProps) {
     return (
-        <div className="bg-icterine md:rounded-d-info flex items-end">
-            <div className="flex-1 flex flex-col justify-center mb-auto p-24">
-                <h2 className="text-left text-4xl md:text-6xl mb-4 md:mb-0 font-bold">{content.title}</h2>
-                <p className="text-base mb-2">{content.description}</p>
-                <p className="text-sm mb-4">{content.text}</p>
-                <DefaultButton text={content.button.text} className="mb-4" bgColor="bg-white" />
+        <div className="bg-icterine rounded-m-info md:rounded-d-info flex">
+            <div className="flex-1 flex flex-col p-12 md:p-24">
+                <h2 className="text-center md:text-left text-4xl md:text-5xl mb-4 md:mb-0 font-bold">{content.title}</h2>
+                <p className="text-left text-xl font-bold py-8">{content.description}</p>
+                <p className="text-center md:text-left text-xl mb-4">{content.text}</p>
+                <DefaultButton text={content.button.text} className="mb-4 self-center mt-auto" bgColor="bg-white" />
             </div>
-            <div className="flex justify-end items-end mt-24 mr-24">
+            <div className="hidden md:flex justify-end items-end pt-16 pr-16">
                 <Image
                     src={guy}
                     fallback={guy_fallback}
