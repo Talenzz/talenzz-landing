71d3617129683af1273fa360801dd9c963e2771c
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-10-01 22:45:34 +0200

    Update page container size

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -4,7 +4,7 @@ interface PageContainerProps {
 
 export function PageContainer({ children }: PageContainerProps) {
     return (
-        <div className="text-center items-center justify-center mx-auto w-10/12">
+        <div className="text-center items-center justify-center mx-auto w-9/12">
             {children}
         </div>
     );
