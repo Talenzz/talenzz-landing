ba53bf001fa841271f743dbc106a618bb4193033
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-10-01 22:47:04 +0200

    Add email form Iframe, Update ApplySection typography

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,17 @@ function App() {
 
                 <ApplySection content={text.apply} />
 
+                <div className="h-20" />
+
+                <InnerPageContainer>
+                    <p className="text-xl text-left">Tretet unserem Newsletter bei, um mit uns die Zukunft der Musikbranche mitzugestalten! Bleibt über alle Entwicklungen informiert und verpasst keine News. Die ersten 100 Anmeldungen kriegen einen Gutschein zum Launch der Plattform im Januar 2024.</p>
+                </InnerPageContainer>
+
+                <iframe
+                width="100%"
+                height="900px"
+                    src="https://a36683bd.sibforms.com/serve/MUIFAGAvr35q-uYbx2ltmVpk3LPRoTM9zgYhi2ffa08AD4-7M0HZCUY8jOTRZSZyZV9keROCtjkV3NoNY-3xyi1vz0X9T15_UQq_KNDrwdfPVRTVTJmT6nqkZxVtlXMouBb25sC33PPQ0yZtPO_2yln0IGg5aFcTIfrQoKpw2yE8HQZeFSC1LZ6jAwQj_rg2duq6MT4cOh4RlfJc"
+                />
             </PageContainer>
             <Footer />
         </>
--- src/components/ApplySection/index.tsx
+++ src/components/ApplySection/index.tsx
@@ -13,7 +13,7 @@ export function ApplySection({ content }: ApplySectionProps) {
         <div className="bg-icterine rounded-m-info md:rounded-d-info flex">
             <div className="flex-1 flex flex-col p-12 md:p-24">
                 <h2 className="text-center md:text-left text-4xl md:text-5xl mb-4 md:mb-0 font-bold">{content.title}</h2>
-                <p className="text-left text-xl font-bold py-8">{content.description}</p>
+                <p className="text-left text-xl py-8">{content.description}</p>
                 <p className="text-center md:text-left text-xl mb-4">{content.text}</p>
                 <DefaultButton text={content.button.text} className="mb-4 self-center mt-auto" bgColor="bg-white" />
             </div>
