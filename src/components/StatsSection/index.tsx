e85be0cddd515aa0bc86f4ad527b38b69d230da5
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-10-02 20:25:32 +0200

    Change font size in StatsSection

--- src/components/StatsSection/index.tsx
+++ src/components/StatsSection/index.tsx
@@ -6,9 +6,9 @@ interface StatsSectionProps {
 export function StatsSection({ info, statsText }: StatsSectionProps) {
     const statsTexts = statsText.map((text, index) => {
         return (
-            <div key={index} className="flex flex-col w-64 text-center lg:text-left mt-12 lg:mt-0">
+            <div key={index} className="flex flex-col w-72 text-center lg:text-left mt-12 lg:mt-0">
                 <span className="text-8xl font-bold">{text.title}</span>
-                <p className=" text-body2 font-bold">{text.text}</p>
+                <p className="font-bold text-md">{text.text}</p>
             </div>
         );
     });
@@ -16,7 +16,7 @@ export function StatsSection({ info, statsText }: StatsSectionProps) {
     return (
         <div className="flex flex-col items-center lg:flex-row lg:justify-between content-center border-y border-black py-4">
             <div className="lg:w-4/12 lg:my-auto">
-                <p className="w-full font-bold text-2xl lg:text-3xl text-center lg:text-left">
+                <p className="w-full font-bold text-2xl lg:text-2xl text-center lg:text-left">
                     {info}
                 </p>
             </div>
