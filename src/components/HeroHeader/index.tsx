89a72f8ebc39700be28bbb375405a9d4886c818a
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-10-16 19:49:48 +0200

    Reduce content to hero, center hero content

--- src/components/HeroHeader/index.tsx
+++ src/components/HeroHeader/index.tsx
@@ -5,7 +5,6 @@ import header_image from "../../assets/images/header_image.webp";
 import header_image_fallback from "../../assets/images/header_image.jpg";
 import { DefaultButton } from "../DefaultButton";
 import { HeroText } from "../../types";
-import { BsArrowRight } from "react-icons/bs";
 
 interface HeroHeaderProps {
     text: HeroText;
@@ -13,19 +12,17 @@ interface HeroHeaderProps {
 
 export function HeroHeader({ text }: HeroHeaderProps) {
     return (
-        <div className="relative" id="hero">
+        <div className="relative h-screen" id="hero">
             <Image
                 src={header_image}
                 fallback={header_image_fallback}
                 alt="Background"
-                className="absolute w-full h-full object-cover object-center brightness-25 z-0"
+                className="absolute w-full h-screen object-cover object-center brightness-25 z-0"
             />
 
             <Header />
 
-            <div className="h-24"></div>
-
-            <div className="relative flex flex-col items-center justify-center h-full z-0 pb-14">
+            <div className="relative flex flex-col items-center justify-center h-full z-0 pb-14 pt-24">
                 <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold text-white flex flex-col items-center justify-center select-none">
                     <div className="flex items-center">
                         <img
@@ -44,18 +41,9 @@ export function HeroHeader({ text }: HeroHeaderProps) {
                     {text.subline}
                 </p>
                 <DefaultButton
-                    text={text.button.text}
+                    text="Coming soon!"
                     bgColor={"bg-white"}
                     className="mb-8 lg:text-xl"
-                    Icon={<BsArrowRight />}
-                    onClick={() => {
-                        // scroll to #apply-section when button is clicked
-                        const applySection =
-                            document.getElementById(text.button.link);
-                        if (applySection) {
-                            applySection.scrollIntoView({ behavior: "smooth" });
-                        }
-                    }}
                 />
             </div>
         </div>
