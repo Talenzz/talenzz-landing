b17fe1bfb6f7b72d66192273c942017c4dc43fad
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-10-16 20:35:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base: '/talenzz-landing/',
-
-  preview: {
-    host: true,
-    port: 5173,
-  }
 })
