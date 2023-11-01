e2bab6b6f1e06550f08cb2355d68ddb6e6dd2421
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-11-01 22:46:10 +0100

    Update imprint and privacy policy layout
    * add black logo
    * change logo position to fit landing-page

--- src/pages/Imprint/index.tsx
+++ src/pages/Imprint/index.tsx
@@ -2,28 +2,31 @@ import Markdown from "react-markdown";
 import { imprintContent } from "../../data/imprint";
 import styles from "./index.module.css";
 import { PageContainer } from "../../components/PageContainer";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo_black.svg";
 import { Link, ScrollRestoration } from "react-router-dom";
 
 export default function ImprintPage() {
     return (
         <>
             <ScrollRestoration />
-            <PageContainer>
-                <div>
+            <div
+                id="header-container"
+                className="top-0 left-0 w-full p-4 z-10 pt-8"
+            >
+                <header id="header" className="md:w-9/12 w-11/12 mx-auto">
                     <Link to="/">
                         <picture>
                             <img
                                 src={logo}
-                                className="logo mx-auto md:mx-0 bg-black"
+                                className="logo mx-auto md:mx-0"
                                 alt="Talenzz logo"
                             />
                         </picture>
                     </Link>
-                    <Markdown className={styles.imprint}>
-                        {imprintContent}
-                    </Markdown>
-                </div>
+                </header>
+            </div>
+            <PageContainer>
+                <Markdown className={styles.imprint}>{imprintContent}</Markdown>
             </PageContainer>
         </>
     );
--- src/pages/PrivacyPolicy/index.tsx
+++ src/pages/PrivacyPolicy/index.tsx
@@ -2,28 +2,33 @@ import { Link, ScrollRestoration } from "react-router-dom";
 import Markdown from "react-markdown";
 import { privacyPolicyContent } from "../../data/privacyPolicy";
 import { PageContainer } from "../../components/PageContainer";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo_black.svg";
 import styles from "./index.module.css";
 
 export default function PrivacyPolicyPage() {
     return (
         <>
             <ScrollRestoration />
-            <PageContainer>
-                <div>
+            <div
+                id="header-container"
+                className="top-0 left-0 w-full p-4 z-10 pt-8"
+            >
+                <header id="header" className="md:w-9/12 w-11/12 mx-auto">
                     <Link to="/">
                         <picture>
                             <img
                                 src={logo}
-                                className="logo mx-auto md:mx-0 bg-black"
+                                className="logo mx-auto md:mx-0"
                                 alt="Talenzz logo"
                             />
                         </picture>
                     </Link>
-                    <Markdown className={styles.privacyPolicy}>
-                        {privacyPolicyContent}
-                    </Markdown>
-                </div>
+                </header>
+            </div>
+            <PageContainer>
+                <Markdown className={styles.privacyPolicy}>
+                    {privacyPolicyContent}
+                </Markdown>
             </PageContainer>
         </>
     );
